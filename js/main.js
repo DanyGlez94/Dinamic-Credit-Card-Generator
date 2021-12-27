8a3aec0e37aa34e84883499c0bdfda2145dcfd18
DanyGlez94 <daniel_glez94@hotmail.com>
2021-12-27 14:58:10 -0600

    Agregado el HTML, los estilos y la funcionalidad del botón para desplegar el formulario

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
-const card = document.querySelector('#card');
+const card = document.querySelector('#card'),
+      cardBtn = document.querySelector('#btn__openForm');
 
 card.addEventListener('click', () => {
     card.classList.toggle('active'); //toggle agrega la clase active si no la tiene, o se la quita si sí la tiene.
+});
+
+cardBtn.addEventListener('click', () => {
+    cardBtn.classList.toggle('active');
 });
