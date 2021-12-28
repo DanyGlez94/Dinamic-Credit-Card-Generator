6220be5c59e814e4365e2c8d3b39ab8a5e25b764
DanyGlez94 <daniel_glez94@hotmail.com>
2021-12-27 19:46:35 -0600

    Input nombre de tarjeta ya cambia de manera dinámica

--- js/main.js
+++ js/main.js
@@ -1,9 +1,10 @@
 const card = document.querySelector('#card'),
       cardBtn = document.querySelector('#btnOpenForm'),
-      cardForm = document.querySelector("#cardForm")
-      cardNumber = document.querySelector('#card .card__number')
-      cardName = document.querySelector('#card .card__name')
-      logoBrand = document.querySelector('#logoBrand');
+      cardForm = document.querySelector("#cardForm"),
+      cardNumber = document.querySelector('#card .card__number'),
+      cardName = document.querySelector('#card .card__name'),
+      logoBrand = document.querySelector('#logoBrand'),
+      signature = document.querySelector('#signature .card__signature p');
 
 //Volteamos la tarjeta para mostrar el frente.
 const showFrontside = () => {
@@ -45,7 +46,7 @@ cardForm.inputNumber.addEventListener('keyup', (e) => { //Con keyup, una vez se 
     let inputValue = e.target.value;
 
     cardForm.inputNumber.value = inputValue
-    .replace(/\s/g, '') //Elimina los espacion con esta expresión regular.
+    .replace(/\s/g, '') //Elimina los espacios con esta expresión regular.
     .replace(/\D/g, '') //Elimina las letras.
     .replace(/([0-9]{4})/g, '$1 ') //Agrega un espacio cada 4 caracteres.
     .trim(); //Elimina el último espacio.
@@ -61,14 +62,31 @@ cardForm.inputNumber.addEventListener('keyup', (e) => { //Con keyup, una vez se 
     if (inputValue[0] == 4) {
         logoBrand.innerHTML = '';
         const image = document.createElement('img');
-        image.src = '/img/logos/visa.png';
+        image.src = 'img/logos/visa.png';
         logoBrand.appendChild(image);
     } else if (inputValue[0] == 5) {
         logoBrand.innerHTML = '';
         const image = document.createElement('img');
-        image.src = '/img/logos/mastercard.png';
+        image.src = 'img/logos/mastercard.png';
         logoBrand.appendChild(image);
     }
     
     showFrontside(); //Volteamos la tarjeta para que el usuario vea el frente.
+});
+
+//Input nombre de tarjeta
+cardForm.inputName.addEventListener('keyup', (e) => {
+    let inputValue = e.target.value;
+
+    cardForm.inputName.value = inputValue
+    .replace(/\d/g, ''); //Elimina los números.
+
+    cardName.textContent = inputValue;
+    signature.textContent = inputValue;
+
+    if (inputValue == '') {
+        cardName.textContent = 'John Doe';
+    }
+
+    showFrontside();
 });
